930c76abb7ac83693dfe16f58e69e90fef7825b0
M7md Odeh <1200089@student.birzeit.edu>
2023-12-30 03:00:17 +0200

    .

--- app/src/main/java/com/example/andriodlabproject/SignUpActivity.java
+++ app/src/main/java/com/example/andriodlabproject/SignUpActivity.java
@@ -14,14 +14,19 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     private Spinner countrySpinner;
     private Spinner citySpinner;
+    private Spinner genderSpinner;
     private String [] cities = null;
     private ImageView imageViewCountryFlag;
     private TextView textViewCountryCode;
+    private boolean firstCountry = true;
+    private boolean firstGender = true;
+    private boolean firstCity = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,17 +58,18 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
         });
 
         // set the gender spinner
-        String[] genders = {"Male", "Female"};
-        final Spinner genderSpinner =(Spinner) findViewById(R.id.spinner_registerGender);
+        String[] genders = {"Gender", "Male", "Female"};
+        genderSpinner =(Spinner) findViewById(R.id.spinner_registerGender);
         ArrayAdapter<String> objGenderArr = new
                 ArrayAdapter<>(this,android.R.layout.simple_spinner_item, genders);
         genderSpinner.setAdapter(objGenderArr);
+        genderSpinner.setOnItemSelectedListener(this);
 
         imageViewCountryFlag = (ImageView) findViewById(R.id.imageView_registerCountryFlag);
         textViewCountryCode = (TextView) findViewById(R.id.textView_CountryCode);
 
         // set the country spinner
-        String [] countries = {"Palestine", "Jordan", "Yemen", "Lebanon", "Syria"};
+        String [] countries = {"Country", "Palestine", "Jordan", "Yemen", "Lebanon", "Syria"};
         countrySpinner =(Spinner) findViewById(R.id.spinner_registerCountry);
         ArrayAdapter<String> objCountryArr = new
                 ArrayAdapter<>(this,android.R.layout.simple_spinner_item, countries);
@@ -71,13 +77,22 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
 
         // set the city spinner
         citySpinner =(Spinner) findViewById(R.id.spinner_registerCity);
+        cities = new String[]{"City"};
+        ArrayAdapter<String> objCityArr = new
+                ArrayAdapter<>(this,android.R.layout.simple_spinner_item, cities);
+        citySpinner.setAdapter(objCityArr);
+
         // set the listener for the country spinner to change the cities when the country is changed
         countrySpinner.setOnItemSelectedListener(this);
+        // set the listener for the city spinner
+        citySpinner.setOnItemSelectedListener(this);
 
-        // make the spinner select nothing at the beginning and show the hint
+
 
 
     }
+
+
 
     /**
      * This method is called when an item in the country spinner is selected
@@ -85,44 +100,92 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
      * @param view the view within the adapter view that was clicked
      * @param pos the position of the view in the adapter
      * @param id the row id of the item that is selected
-     * function : change the cities in the city spinner and change the country code and flag
+     * function : change the cities in the city spinner when the country is changed
      */
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-        if (pos == 0) { // Palestine
-            // add cities of Palestine and set the country code and flag
-            cities = new String[]{"Gaza", "Tulkarm", "Ramallah", "Hebron", "Jerusalem", "Jenin", "Nablus", "Qalqilya", "Tubas", "Salfit", "Bethlehem", "Jericho"};
-            imageViewCountryFlag.setImageResource(R.drawable.palestine);
-            textViewCountryCode.setText("+970");
-        }
-        else if (pos == 1) { // Jordan
-            // add cities of Jordan and set the country code and flag
-            cities = new String[]{"Amman", "Irbid", "Zarqa", "Aqaba", "Salt", "Madaba", "Karak", "Ma'an", "Tafilah", "Mafraq", "Jerash", "Ajloun"};
-            imageViewCountryFlag.setImageResource(R.drawable.jordan);
-            textViewCountryCode.setText("+962");
-        }
-        else if (pos == 2) { // Yemen
-            // add cities of Yemen and set the country code and flag
-            cities = new String[]{"Sana'a", "Aden", "Taiz", "Al Hudaydah", "Ibb", "Dhamar", "Al Mukalla", "Zinjibar", "Sayyan", "Ash Shihr", "Sahar", "Hajjah"};
-            imageViewCountryFlag.setImageResource(R.drawable.yemen);
-            textViewCountryCode.setText("+967");
-        }
-        else if (pos == 3) { // Lebanon
-            // add cities of Lebanon and set the country code and flag
-            cities = new String[]{"Beirut", "Tripoli", "Sidon", "Tyre", "Jounieh", "Zahle", "Byblos", "Baalbek", "Batroun", "Aley", "Nabatieh", "Jbeil"};
-            imageViewCountryFlag.setImageResource(R.drawable.lebanon);
-            textViewCountryCode.setText("+961");
-        }
-        else if (pos == 4) { // Syria
-            // add cities of Syria
-            cities = new String[]{"Damascus", "Aleppo", "Homs", "Hama", "Latakia", "Deir ez-Zor", "Ar-Raqqah", "Al-Hasakah", "Qamishli", "Idlib", "Daraa", "As-Suwayda"};
-            imageViewCountryFlag.setImageResource(R.drawable.syria);
-            textViewCountryCode.setText("+963");
+        // if this for the country spinner
+        if (parent.getId()==R.id.spinner_registerCountry){
+            if (firstCountry) {
+                // change the color of the hint
+                ((TextView) parent.getChildAt(0)).setTextColor(getResources().getColor(R.color.grey));
+                firstCountry = false;
+                return;
+            } else{
+                if (pos == 0) {
+                    // show toast to select a country
+                    Toast.makeText(this, "Please select a country", Toast.LENGTH_SHORT).show();
+                    countrySpinner.setSelection(1);
+                }
+                if (pos == 1) { // Palestine
+                    // add cities of Palestine and set the country code and flag
+                    cities = new String[]{"City", "Gaza", "Tulkarm", "Ramallah", "Hebron", "Jerusalem", "Jenin", "Nablus", "Qalqilya", "Tubas", "Salfit", "Bethlehem", "Jericho"};
+                    imageViewCountryFlag.setImageResource(R.drawable.palestine);
+                    textViewCountryCode.setText("+970");
+                }
+                else if (pos == 2) { // Jordan
+                    // add cities of Jordan and set the country code and flag
+                    cities = new String[]{"City", "Amman", "Irbid", "Zarqa", "Aqaba", "Salt", "Madaba", "Karak", "Ma'an", "Tafilah", "Mafraq", "Jerash", "Ajloun"};
+                    imageViewCountryFlag.setImageResource(R.drawable.jordan);
+                    textViewCountryCode.setText("+962");
+                }
+                else if (pos == 3) { // Yemen
+                    // add cities of Yemen and set the country code and flag
+                    cities = new String[]{"City", "Sana'a", "Aden", "Taiz", "Al Hudaydah", "Ibb", "Dhamar", "Al Mukalla", "Zinjibar", "Sayyan", "Ash Shihr", "Sahar", "Hajjah"};
+                    imageViewCountryFlag.setImageResource(R.drawable.yemen);
+                    textViewCountryCode.setText("+967");
+                }
+                else if (pos == 4) { // Lebanon
+                    // add cities of Lebanon and set the country code and flag
+                    cities = new String[]{"City", "Beirut", "Tripoli", "Sidon", "Tyre", "Jounieh", "Zahle", "Byblos", "Baalbek", "Batroun", "Aley", "Nabatieh", "Jbeil"};
+                    imageViewCountryFlag.setImageResource(R.drawable.lebanon);
+                    textViewCountryCode.setText("+961");
+                }
+                else if (pos == 5) { // Syria
+                    // add cities of Syria
+                    cities = new String[]{"City", "Damascus", "Aleppo", "Homs", "Hama", "Latakia", "Deir ez-Zor", "Ar-Raqqah", "Al-Hasakah", "Qamishli", "Idlib", "Daraa", "As-Suwayda"};
+                    imageViewCountryFlag.setImageResource(R.drawable.syria);
+                    textViewCountryCode.setText("+963");
+
+                }
+                ArrayAdapter<String> objCityArr = new
+                        ArrayAdapter<>(this,android.R.layout.simple_spinner_item, cities);
+                citySpinner.setAdapter(objCityArr);
+                firstCity = true;
+
+            }
 
         }
-        ArrayAdapter<String> objCityArr = new
-                ArrayAdapter<>(this,android.R.layout.simple_spinner_item, cities);
-        citySpinner.setAdapter(objCityArr);
+        // if this for the city spinner
+        else if (parent.getId()==R.id.spinner_registerCity) {
+            if (firstCity) {
+                // change the color of the hint
+                ((TextView) parent.getChildAt(0)).setTextColor(getResources().getColor(R.color.grey));
+                firstCity = false;
+            } else {
+                if (pos == 0) {
+                    // show toast to select a city
+                    Toast.makeText(this, "Please select a city", Toast.LENGTH_SHORT).show();
+                    citySpinner.setSelection(1);
+                }
+            }
+        }
+        // if this for the gender spinner
+        else if (parent.getId() == R.id.spinner_registerGender){
+            if (firstGender){
+                // change the color of the hint
+                ((TextView) parent.getChildAt(0)).setTextColor(getResources().getColor(R.color.grey));
+                firstGender = false;
+
+            } else {
+                if (pos == 0){
+                    // show toast to select Gender
+                    Toast.makeText(this, "Please select a Gender", Toast.LENGTH_SHORT).show();
+                    genderSpinner.setSelection(1);
+                }
+            }
+        }
+
     }
 
     @Override
