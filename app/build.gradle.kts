748a0d6c9aad589312abe4c48fdb8a183f2a5e52
Osaid Hamza <108472343+OsaidHamza7@users.noreply.github.com>
2024-01-21 19:39:40 +0200

    Add ProgressBar For Connection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("com.google.android.material:material:1.6.0")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
--- app/src/main/java/com/example/andriodlabproject/MainActivity.java
+++ app/src/main/java/com/example/andriodlabproject/MainActivity.java
@@ -4,22 +4,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements AsyncTaskCallback{
     private DataBaseHelper dataBaseHelper;
+    private ProgressBar progressBar;
+    private View screenOverlay;
+
     String httpRequest="https://658582eb022766bcb8c8c86e.mockapi.io/api/mock/rest-apis/encs5150/car-types";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button connect= findViewById(R.id.button_Connect);
+        progressBar = findViewById(R.id.barConnection);
+        screenOverlay = findViewById(R.id.screenOverlay);
 
         dataBaseHelper = new DataBaseHelper(this);
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                showProgressBar(true); // show the progress bar
                 ConnectionAsyncTask connectionAsyncTask = new
                         ConnectionAsyncTask(MainActivity.this,MainActivity.this);
                 connectionAsyncTask.execute(httpRequest);
@@ -35,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements AsyncTaskCallback
     public void onTaskComplete(boolean success) {
         if (success) {
             // if the connection is succeeded
+            showProgressBar(false); // hide the progress bar
             Intent intent = new Intent(MainActivity.this, SignInActivity.class);
             MainActivity.this.startActivity(intent);
         }
@@ -42,5 +50,15 @@ public class MainActivity extends AppCompatActivity implements AsyncTaskCallback
 
     public DataBaseHelper getDatabaseHelper() {
         return dataBaseHelper;
+    }
+
+    private void showProgressBar(boolean show) {
+        if (show) {
+            progressBar.setVisibility(View.VISIBLE);
+            screenOverlay.setVisibility(View.VISIBLE); // Show the overlay
+        } else {
+            progressBar.setVisibility(View.GONE);
+            screenOverlay.setVisibility(View.GONE); // Hide the overlay
+        }
     }
 }
